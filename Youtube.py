1cff78cdc6f7c50ab6ad827db1b41f444ef6501f
Steven TH Yu <Steven.TH.Yu@BenQ.com>
2020-06-26 16:47:04 +0800

    add expection

--- Youtube.py
+++ Youtube.py
@@ -20,9 +20,6 @@ import pdb
 
 
 countries = []
-sb_youtube_info = []
-youtube_info = []
-
 while True:
 	country = input('Please enter the countries: ')
 
@@ -33,37 +30,46 @@ while True:
 	
 rows = int(input('Please enter the number within the top ranks: '))
 
+
+# sb_youtube_info
+sb_youtube_info = []
+
 for country_select in countries:
-	url = 'https://socialblade.com/youtube/top/country/' + str(country_select)
-	headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-	time.sleep(1)
-	response = requests.get(url, headers=headers)
-	soup = BeautifulSoup(response.text, 'lxml')   # 使用lxml解析器, 回傳內容裡的文字, 也就是html內容傳入BeautifulSoup
-	
-	table_soup = soup.find(style = re.compile('float: right; width'))
-	row_soup = table_soup.find_all(style = re.compile('color:#444'))
-	
-	for row_data in row_soup:
+	try:
+		url = 'https://socialblade.com/youtube/top/country/' + str(country_select)
+		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+		time.sleep(1)
+		response = requests.get(url, headers=headers)
+		soup = BeautifulSoup(response.text, 'lxml')   # 使用lxml解析器, 回傳內容裡的文字, 也就是html內容傳入BeautifulSoup
 
-		if row_soup.index(row_data) <= rows-1 :
-			sb_rank_row = row_data.find(style = re.compile('color:#888'))
+	except BaseException as error:
+		print('Expection Error:', error)
 
-			# 去掉sb_rank後面的2個字母
-			sb_rank_row_del_text = list(sb_rank_row.getText())[:-2]
-			sb_rank = int(''.join(sb_rank_row_del_text))     # list -> str -> int
-						
-			sb_url_row = row_data.find(href = re.compile('/youtube/user/')).attrs['href']
-			sb_url = 'https://socialblade.com' + str(sb_url_row)
-					
-		else:
-			break
-
-		sb_link = [country_select, sb_rank, sb_url]
-		sb_youtube_info.append(sb_link)
+	else:
+		table_soup = soup.find(style = re.compile('float: right; width'))
+		row_soup = table_soup.find_all(style = re.compile('color:#444'))
 		
+		for row_data in row_soup:
+
+			if row_soup.index(row_data) <= rows-1 :
+				sb_rank_row = row_data.find(style = re.compile('color:#888'))
+
+				# 去掉sb_rank後面的2個字母
+				sb_rank_row_del_text = list(sb_rank_row.getText())[:-2]
+				sb_rank = int(''.join(sb_rank_row_del_text))     # list -> str -> int
+							
+				sb_url_row = row_data.find(href = re.compile('/youtube/user/')).attrs['href']
+				sb_url = 'https://socialblade.com' + str(sb_url_row)
+						
+			else:
+				break
+
+			sb_link = [country_select, sb_rank, sb_url]
+			sb_youtube_info.append(sb_link)
+			
 # to DataFrame
 sb_youtube_info_df = pd.DataFrame(sb_youtube_info, columns=['country_select', 'sb_rank', 'sb_url'])
-		
+			
 # to excel file
 sb_youtube_info_df.to_csv('sb_youtube_info.csv', index=False, sep=',')
 
@@ -73,6 +79,8 @@ with sqlite3.connect('youtubers.db') as conn:
 
 
 
+# youtube_info
+youtube_info = []
 
 # 開啟sqlite 連結
 with sqlite3.connect('youtubers.db') as conn:
@@ -82,33 +90,38 @@ with sqlite3.connect('youtubers.db') as conn:
 sb_youtube_info = data.values.tolist()
 
 for sb_youtube_info_line in sb_youtube_info:
-	country = sb_youtube_info_line[0]
-	sb_rank = sb_youtube_info_line[1]
-	url = sb_youtube_info_line[2]
 
-	headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-	time.sleep(1)
-	response = requests.get(url, headers=headers)
-	soup = BeautifulSoup(response.text, 'lxml')
+	try:
+		country = sb_youtube_info_line[0]
+		sb_rank = sb_youtube_info_line[1]
+		url = sb_youtube_info_line[2]
+		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+		time.sleep(1)
+		response = requests.get(url, headers=headers)
+		soup = BeautifulSoup(response.text, 'lxml')   # 使用lxml解析器, 回傳內容裡的文字, 也就是html內容傳入BeautifulSoup
 
-	channel_url = soup.select('#YouTubeUserTopSocial > div > .-margin')[0].attrs['href']
+	except BaseException as error:
+		print('Expection Error:', error)
 
-	top_soup = soup.select('#YouTubeUserTopInfoBlockTop')[0]
-	channel_name = top_soup.select('div > h1')[0].text
-	channel_type = top_soup.select('#youtube-user-page-channeltype')[0].text
-	uploads = top_soup.select('#youtube-stats-header-uploads')[0].text
-	subs = top_soup.select('#youtube-stats-header-subs')[0].text
-	video_views = top_soup.select('#youtube-stats-header-views')[0].text
+	else:
+		channel_url = soup.select('#YouTubeUserTopSocial > div > .-margin')[0].attrs['href']
 
-	rank_info = soup.find(style = re.compile('float: right; width'))
-	type_rank = rank_info.select('div:nth-child(5) > div:nth-child(1) > p')[0].text
+		top_soup = soup.select('#YouTubeUserTopInfoBlockTop')[0]
+		channel_name = top_soup.select('div > h1')[0].text
+		channel_type = top_soup.select('#youtube-user-page-channeltype')[0].text
+		uploads = top_soup.select('#youtube-stats-header-uploads')[0].text
+		subs = top_soup.select('#youtube-stats-header-subs')[0].text
+		video_views = top_soup.select('#youtube-stats-header-views')[0].text
 
-	youtube_link = [country, sb_rank, channel_name, channel_type, type_rank, uploads, subs, video_views, channel_url]
-	youtube_info.append(youtube_link)
-	
+		rank_info = soup.find(style = re.compile('float: right; width'))
+		type_rank = rank_info.select('div:nth-child(5) > div:nth-child(1) > p')[0].text
+
+		youtube_link = [country, sb_rank, channel_name, channel_type, type_rank, uploads, subs, video_views, channel_url]
+		youtube_info.append(youtube_link)
+		
 # to DataFrame
 youtube_info_df = pd.DataFrame(youtube_info, columns=['country', 'sb_rank', 'channel_name', 'channel_type', 'type_rank', 'uploads', 'subs', 'video_views', 'channel_url'])
-	
+		
 # to excel file
 youtube_info_df.to_csv('youtube_info.csv', index=False, sep=',')
 
